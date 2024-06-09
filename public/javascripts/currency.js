27d4eef1efca90daab476712e0221a272acaa1db
Michael McMillen <michael.mc09@gmail.com>
2024-06-09 13:06:31 +0100

    Add concat for full currency symbols

--- public/javascripts/currency.js
+++ public/javascripts/currency.js
@@ -2,13 +2,25 @@ var countryInput = document.getElementById('country-input');
 var goBtn = document.getElementById('go-btn');
 goBtn.addEventListener('click', go);
 
-let currencyText = document.getElementsByTagName('p')[0];
+let currencySymbol = document.getElementById('currencySymbol');
 
 function go() {
     fetch(`http://localhost:3000/currency/countryCurrency/${countryInput.value}`)
-    .then(response => response.json())
-    .then(resp => resp[0].symbol)
-    .then(currency => String.fromCharCode(parseInt(currency, 16)))
-    .then(currsym => currencyText.textContent = currsym);
-    }
+        .then(response => response.json())
+        .then(resp => resp[0].symbol)
+        .then(symbol => {
+            let symArray = []
+            if (symbol.indexOf(',') > -1) {
+                symArray = symbol.split(',');
+                let renderedSymbol = '';
+                for (let i = 0; i < symArray.length; i++) {
+                    renderedSymbol = renderedSymbol + String.fromCharCode(parseInt(symArray[i], 16));
+                }
+                currencySymbol.textContent = renderedSymbol
+            }
+            else {
+                currencySymbol.textContent = String.fromCharCode(parseInt(symbol, 16));
+            };
+        })
+};
 
