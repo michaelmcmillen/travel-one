2ef475724550a856be2c05995a577dc841f3b4a8
Michael McMillen <michael.mc09@gmail.com>
2024-09-08 11:33:00 +0100

    fix: Remove unused npm packages

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 var app = express();
 
-// View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
